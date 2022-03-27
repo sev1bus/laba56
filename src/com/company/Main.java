b2ae758246df932785717d36342e9c4028164f62
sev1bus <101794447+sev1bus@users.noreply.github.com>
2022-03-27 23:11:32 +0300

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Завдання 1
+        int n = 90;
+
+        if (n > 60 && n < 130)
+            System.out.print("Завдання 1: Число " +n+ "  належить проміжку(60, 130)");
+
+        else System.out.print("Завдання 1: Число " +n+ " не належить проміжку(60, 130)");
+
+    }
+}
